ea8b0ce5d0a7beca536101906940dc72a73a5020
Daniël Klabbers <luceos@users.noreply.github.com>
2024-06-03 21:20:05 +0200

    Update ApplyUserSortingMiddleware.php

--- src/ApplyUserSortingMiddleware.php
+++ src/ApplyUserSortingMiddleware.php
@@ -8,15 +8,12 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Log\LoggerInterface;
 
 class ApplyUserSortingMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $actor = RequestUtil::getActor($request);
-
-        resolve(LoggerInterface::class)->info('sup');
 
         if ($request->getUri()->getPath() !== '/') {
             return $handler->handle($request);
